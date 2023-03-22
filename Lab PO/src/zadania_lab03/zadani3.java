8d4a1ae1f755339bec2c7aeed1f3dd93c9d2bb08
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2023-03-22 18:12:01 +0100

    .

---
+++ Lab PO/src/zadania_lab03/zadani3.java
@@ -0,0 +1,30 @@
+package zadania_lab03;
+
+import java.util.Scanner;
+
+public class zadani3 {
+    public static void main(String[] args) {
+        int n = evenNumbersCounter();
+        System.out.println("Suma liczb parzystych w podanym ciągu wynosi: " +n);
+    }
+
+    public static int evenNumbersCounter(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Podaj długość ciągu liczba musi byc dodatnia: ");
+        int N;
+        N = input.nextInt();
+        int counter = 0;
+        if(N > 0){
+            int i = 1;
+            do {
+                if(i%2 == 0){
+                    counter = counter + i;
+                }
+                ++i;
+            }while( i <= N);
+        }else {
+            evenNumbersCounter();
+        }
+        return counter;
+    }
+}
