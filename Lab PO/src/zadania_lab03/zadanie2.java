00add32bde1d916e75571d399cfc07678f7ac0ee
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2023-03-22 17:56:15 +0100

    .

--- Lab PO/src/zadania_lab03/zadanie2.java
+++ Lab PO/src/zadania_lab03/zadanie2.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class zadanie2 {
     public static void main(String[] args) {
         int[] results = countNumbers();
-        System.out.println("Liczb dodatnich jest " +results[0]+ " a liczb ujemnych "+results[1]+ " łącznie takich liczb jest " +(results[0]+results[1]));
+        System.out.println("Liczb dodatnich jest: " +results[0]+ ", suma dodatnich elementów to: " +results[3]+ ", a liczb ujemnych: " +results[1]+ " suma umjemnych elementów " +results[2] );
     }
 
+    //creating new Array and input numbers
     public static int[] newArray(){
         int[] numbers = new int[10];
         Scanner input = new Scanner(System.in);
@@ -18,20 +19,25 @@ public class zadanie2 {
         return numbers;
     }
 
+    //counting if elements are negative or positive, returning array with results
     public static int[] countNumbers(){
-        int negativeN = 0;
-        int positiveN = 0;
-        int[] counter = new int[2];
+        int negativeN = 0, positiveN = 0;
+        int countPositiveN = 0 , countNegativeN = 0;
+        int[] counter = new int[4];
         int[] arrayN = newArray();
-        for (int j : arrayN) {
-            if (j < 0) {
+        for (int i =0; i < arrayN.length; i++) {
+            if (arrayN[i] < 0) {
                 ++negativeN;
-            } else if (j > 0) {
+                countNegativeN = arrayN[i] + countNegativeN;
+            } else if (arrayN[i] > 0) {
                 ++positiveN;
+                countPositiveN = arrayN[i] + countPositiveN;
             }
         }
         counter[0] = positiveN;
         counter[1] = negativeN;
+        counter[2] = countNegativeN;
+        counter[3] = countPositiveN;
         return counter;
     }
 }
