df11d635774a85425a60be86a6b7e6e9a2dcebef
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2023-06-10 18:23:02 +0200

    nadrobione zadania :)

---
+++ Lab PO/src/labs/odczyt_pliku/Main.java
@@ -0,0 +1,18 @@
+package labs.odczyt_pliku;
+
+
+
+
+public class Main {
+//    public static void main(String[] args) throws IOException {
+//        TextFile TextFilee = new TextFile();
+//        TextFilee.ReadFile();
+//        TextFilee.SaveFile();
+//        TextFilee.ReadFile();
+//        TextFilee.SaveFileWrite();
+//        TextFilee.ReadFile();
+
+       // int number = 123123123;
+        
+    }
+}
