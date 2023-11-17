5e47fb2c6b44b3574c887df0497b62f61900975f
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2023-11-17 15:34:05 +0100

    AiSD - BacteriaProblem-changed

--- AiSD/src/main/java/BacteriaProblem/BacteriaProblemDYN.java
+++ AiSD/src/main/java/BacteriaProblem/BacteriaProblemDYN.java
@@ -1,29 +1,29 @@
 package BacteriaProblem;
 
 public class BacteriaProblemDYN {
+    static int time = 0;
     public static void main(String[] args) {
         bacteria_counter();
     }
     static void bacteria_counter(){
-        int i = 1;
-        int time = 0;
         boolean[] milestone = new boolean[4];
-        while(i < 10000000) {
-            time++;
-            if (i >= 1000 && !milestone[0]) {
+        int[] bacteriaArr = {1};
+        while(true) {
+            if (bacteriaArr[0] >= 1000 && !milestone[0]) {
                 milestone[0] = true;
-                System.out.println("Bacteria number passed 1000 in " + time * 2 + " minutes and count = " + i);
-            } else if (i >= 10000 && !milestone[1]) {
+                System.out.println("Bacteria number passed 1000 in " + time * 2 + " minutes and count = " + bacteriaArr[0]);
+            } else if (bacteriaArr[0] >= 10000 && !milestone[1]) {
                 milestone[1] = true;
-                System.out.println("Bacteria number passed 10000 in " + time * 2 + " minutes and count = " + i);
-            } else if (i >= 100000 && !milestone[2]) {
+                System.out.println("Bacteria number passed 10000 in " + time * 2 + " minutes and count = " + bacteriaArr[0]);
+            } else if (bacteriaArr[0] >= 100000 && !milestone[2]) {
                 milestone[2] = true;
-                System.out.println("Bacteria number passed 100000 in " + time * 2 + " minutes and count = " + i);
-            } else if (i >= 1000000) {
-                System.out.println("Bacteria number passed 1 mil in " + time * 2 + " minutes and count = " + i);
+                System.out.println("Bacteria number passed 100000 in " + time * 2 + " minutes and count = " + bacteriaArr[0]);
+            } else if (bacteriaArr[0] >= 1000000) {
+                System.out.println("Bacteria number passed 1 mil in " + time * 2 + " minutes and count = " + bacteriaArr[0]);
                 break;
             }
-            i = i + i * 2;
+            time++;
+            bacteriaArr[0] = bacteriaArr[0] + bacteriaArr[0]*2;
         }
     }
 }
