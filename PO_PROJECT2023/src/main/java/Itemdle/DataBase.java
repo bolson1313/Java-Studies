134d27c069fe378a9ed21d9be17a014f6c2aeede
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-01-08 23:45:32 +0100

    itemdleFX - inserts stats and small changes

--- PO_PROJECT2023/src/main/java/Itemdle/DataBase.java
+++ PO_PROJECT2023/src/main/java/Itemdle/DataBase.java
@@ -1,7 +1,6 @@
 package Itemdle;
 
 
-import org.controlsfx.control.PropertySheet;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,11 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class DBexecute {
+public class DataBase {
     private final SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
-
-    public DBexecute() {
+    public DataBase() {
     }
 
     public ArrayList<String> getItemsToArray() {
@@ -40,9 +38,17 @@ public class DBexecute {
             return itmList;
         }
     }
+
+    public List<ItemsEntity> getEntityToList(){
+        try(Session session = factory.openSession()){
+            Query query = session.createQuery("from ItemsEntity");
+            List<ItemsEntity> itmList = query.list();
+            return itmList;
+        }
+    }
     public List<ItemsEntity> getRandomItem(int index){
         try(Session session = factory.openSession()){
-            Query query = session.createQuery("from ItemsEntity IE where IE.id="+index);
+            Query query = session.createQuery("from ItemsEntity IE where IE.item_id="+index);
             List<ItemsEntity> itmList = query.list();
             //System.out.println(itmList.toString());
             return itmList;
@@ -58,12 +64,12 @@ public class DBexecute {
         }
     }
 
-    public void insertStat(Stats stats){
+
+    public void saveStats(StatsEntity stats){
         try(Session session = factory.openSession()){
-            Transaction transaction = session.beginTransaction();
-            Query query = session.createQuery("INSERT StatsEntity (id, itemName, tries, datetime) VALUES (null, '"+stats.getItemName()+"', "+stats.getTries()+", '"+stats.getDatetime()+"')");
-            query.executeUpdate();
-            transaction.commit();
+            Transaction tx = session.beginTransaction();
+            session.save(stats);
+            tx.commit();
         }
     }
 }
