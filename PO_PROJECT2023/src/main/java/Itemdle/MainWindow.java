39955f317f8372ebf5c74f80f8b2f37176ad0951
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-01-08 23:47:28 +0100

    itemdleFX - deleted comments and style linking

--- PO_PROJECT2023/src/main/java/Itemdle/MainWindow.java
+++ PO_PROJECT2023/src/main/java/Itemdle/MainWindow.java
@@ -8,21 +8,19 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.List;
 import java.util.Objects;
 
 public class MainWindow extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        new DBexecute();
-        //tu dla testu otwieram gamewindow.fxml, proba naprawy bledu
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main" +
-                "Window.fxml")));
+        new DataBase();
+        //Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindow.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindow.fxml")));
         Scene mainScene = new Scene(root, Color.LIGHTBLUE);
 
-        //style
-        //mainScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+
+
 
 
         //title and window icon
