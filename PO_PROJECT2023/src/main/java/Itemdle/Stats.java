615a051930ef9a278b79aa6c279f03e35407f5cc
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-01-08 23:49:21 +0100

    itemdleFX - hotfix

--- PO_PROJECT2023/src/main/java/Itemdle/Stats.java
+++ PO_PROJECT2023/src/main/java/Itemdle/Stats.java
@@ -8,12 +8,15 @@ public class Stats {
     private int id;
 
     private String itemName;
+    private ItemsEntity guessed_item_id;
     private int tries;
 
     private String datetime;
 
-    public Stats(String itemName, int tries, String datetime) {
+    public Stats(int id, String itemName, ItemsEntity guessed_item_id, int tries, String datetime) {
+        this.id = id;
         this.itemName = itemName;
+        this.guessed_item_id = guessed_item_id;
         this.tries = tries;
         this.datetime = datetime;
     }
@@ -24,6 +27,10 @@ public class Stats {
 
     public String getItemName() {
         return itemName;
+    }
+
+    public ItemsEntity getGuessed_item_id() {
+        return guessed_item_id;
     }
 
     public int getTries() {
